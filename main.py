0932b7ce247afcb0526f0c6b74548a28aacca8b9
Pablo <pablo.sanchi.herrera@gmail.com>
2023-03-19 23:06:08 +0100

    add doc info web page

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
 from flask_cors import CORS
 from tictactoe import TicTacToe
 import json
 
-# create the Flask app
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route("/", methods=['GET'])
 def root():
-    return {"message": "TicTacToe AI"}
+    return redirect("/docs")
 
-@app.route("/aimove", methods=['POST'])
+@app.route("/docs", methods=['GET'])
+def docs():
+    return render_template("index.html")
+
+@app.route("/aimove", methods=['GET'])
 def aimove(): # request : Request
     board = request.json
     board = board['board']
@@ -31,11 +34,11 @@ def aimove(): # request : Request
         "winner": tictactoe.get_winner()
     }
 
-@app.route("/playermove", methods=['POST'])
-def playermove():
-    # get the body
-    board = request.json
-    print(board)
+# @app.route("/playermove", methods=['POST'])
+# def playermove():
+#     # get the body
+#     board = request.json
+#     print(board)
 
 if __name__ == "__main__":
     app.run()
